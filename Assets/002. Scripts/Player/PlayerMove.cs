f5d9d2e013277d2e2a0a0c27be9a4ef8654dfce1
KoSeungYeon <rhtmddus0121@naver.com>
2024-02-13 17:56:40 +0900

    실습 과제 1. Shift 누르고 있으면 빨리 뛰기 (이동 속도 10)
    실습 과제 2. 스태미너 구현
    - Shfit 누른 동안에는 스태미나가 서서히 소모된다.
    - 아니면 스태미나가 소모 되는 속도보다 빠른 속도로 충전된다.
    
    실습 과제 3. 왼쪽 하단에 스태미너 UI 구현 (Slider 활용)

--- Assets/002. Scripts/Player/PlayerMove.cs
+++ Assets/002. Scripts/Player/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -8,13 +9,21 @@ public class PlayerMove : MonoBehaviour
     // 속성 : 
     // - 이동속도
     public float MoveSpeed = 3;
+    public float RunSpeed   = 10;
+
+    // 스태미너와 관련된 변수
+    public float currentStamina;    // 현재 스태미너
+    public float minStamina = 0;
+    public float maxStamina = 10;
+
     // 구현 순서:
     // 1. 키 입력 받기
     // 2. '캐릭터가 바라보는 방향'을 기준으로 방향 구하기
     // 3. 이동하기
     void Start()
     {
-       
+        MoveSpeed = 3;
+        currentStamina = maxStamina; // 시작 시 스태미너를 최대치로 설정
     }
 
     void Update()
@@ -27,7 +36,27 @@ public class PlayerMove : MonoBehaviour
         dir.Normalize();
         // Transforms direction from local space to world space.
         dir = Camera.main.transform.TransformDirection(dir);  // 글로벌 좌표계 (세상의 동서남북)
+
         // 3. 이동하기
         transform.position += MoveSpeed * dir * Time.deltaTime;
+
+        // 현재 스태미너를 minStamina와 maxStamina 사이의 값으로 제한
+        currentStamina = Mathf.Clamp(currentStamina, minStamina, maxStamina);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            MoveSpeed = 10;
+            currentStamina -= Time.deltaTime;
+        }
+        else
+        {
+            MoveSpeed = 3;
+            currentStamina += Time.deltaTime * 2;
+        }
+        if(currentStamina <= 0)
+        {
+            MoveSpeed = 3;
+        }
     }
 }
+
