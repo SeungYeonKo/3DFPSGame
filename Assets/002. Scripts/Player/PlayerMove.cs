05859c4537d17d19127c47959634f2beaba18a94
KoSeungYeon <rhtmddus0121@naver.com>
2024-02-14 13:14:30 +0900

    fps,tps카메라 숫자키누르면 변경 추가 + 스태미나 코드  수정

--- Assets/002. Scripts/Player/PlayerMove.cs
+++ Assets/002. Scripts/Player/PlayerMove.cs
@@ -1,62 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
-    // 목표 : 키보드 방향키(wasd)를 누르면 캐릭터가 바라보는 방향으로 이동시키고 싶다
-    // 속성 : 
+    // 목표: 키보드 방향키(wasd)를 누르면 캐릭터를 바라보는 방향 기준으로 이동시키고 싶다. 
+    // 속성:
     // - 이동속도
-    public float MoveSpeed = 3;
-    public float RunSpeed   = 10;
+    public float MoveSpeed = 5;     // 일반 속도
+    public float RunSpeed = 10;    // 뛰는 속도
 
-    // 스태미너와 관련된 변수
-    public float currentStamina;    // 현재 스태미너
-    public float minStamina = 0;
-    public float maxStamina = 10;
+    public float Stamina = 100;             // 스태미나
+    public const float MaxStamina = 100;    // 스태미나 최대량
+    public float StaminaConsumeSpeed = 33f; // 초당 스태미나 소모량
+    public float StaminaChargeSpeed = 50;  // 초당 스태미나 충전량
 
-    // 구현 순서:
-    // 1. 키 입력 받기
-    // 2. '캐릭터가 바라보는 방향'을 기준으로 방향 구하기
-    // 3. 이동하기
-    void Start()
+    [Header("스태미나 슬라이더 UI")]
+    public Slider StaminaSliderUI;
+
+    private void Start()
     {
-        MoveSpeed = 3;
-        currentStamina = maxStamina; // 시작 시 스태미너를 최대치로 설정
+        Stamina = MaxStamina;
     }
+
+    // 구현 순서
+    // 1. 키 입력 받기
+    // 2. '캐릭터가 바라보는 방향'을 기준으로 방향구하기
+    // 3. 이동하기
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            // FPS 카메라 모드로 전환
+            CameraManager.Instance.SetCameraMode(CameraMode.FPS);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            // TPS 카메라 모드로 전환
+            CameraManager.Instance.SetCameraMode(CameraMode.TPS);
+        }
+
+
         // 1. 키 입력 받기
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        // 2.  '캐릭터가 바라보는 방향'을 기준으로 방향 구하기
-        Vector3 dir = new Vector3(h, 0, v);     // 로컬 좌표계 (나만의 동서남북)
+
+        // 2. '캐릭터가 바라보는 방향'을 기준으로 방향구하기
+        Vector3 dir = new Vector3(h, 0, v);             // 로컬 좌표꼐 (나만의 동서남북) 
         dir.Normalize();
         // Transforms direction from local space to world space.
-        dir = Camera.main.transform.TransformDirection(dir);  // 글로벌 좌표계 (세상의 동서남북)
+        dir = Camera.main.transform.TransformDirection(dir); // 글로벌 좌표계 (세상의 동서남북)
 
-        // 3. 이동하기
-        transform.position += MoveSpeed * dir * Time.deltaTime;
-
-        // 현재 스태미너를 minStamina와 maxStamina 사이의 값으로 제한
-        currentStamina = Mathf.Clamp(currentStamina, minStamina, maxStamina);
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        // 실습 과제 1. Shift 누르고 있으면 빨리 뛰기
+        float speed = MoveSpeed; // 5
+        if (Input.GetKey(KeyCode.LeftShift)) // 실습 과제 2. 스태미너 구현
         {
-            MoveSpeed = 10;
-            currentStamina -= Time.deltaTime;
+            // - Shfit 누른 동안에는 스태미나가 서서히 소모된다. (3초)
+            Stamina -= StaminaConsumeSpeed * Time.deltaTime; // 초당 33씩 소모
+            if (Stamina > 0)
+            {
+                speed = RunSpeed;
+            }
         }
         else
         {
-            MoveSpeed = 3;
-            currentStamina += Time.deltaTime * 2;
+            // - 아니면 스태미나가 소모 되는 속도보다 빠른 속도로 충전된다 (2초)
+            Stamina += StaminaChargeSpeed * Time.deltaTime; // 초당 50씩 충전
         }
-        if(currentStamina <= 0)
-        {
-            MoveSpeed = 3;
-        }
+
+        Stamina = Mathf.Clamp(Stamina, 0, 100);
+        StaminaSliderUI.value = Stamina / MaxStamina;  // 0 ~ 1;//
+
+        // 3. 이동하기
+        transform.position += speed * dir * Time.deltaTime;
     }
 }
-
