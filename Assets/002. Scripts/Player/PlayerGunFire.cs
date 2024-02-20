0bf20fec5f30a2feb2b581c7147870d0c52e2916
SeungYeonKo <createNewScene479@gmail.com>
2024-02-20 17:29:41 +0900

    줌모드에서 무기 교체 시 줌모드 취소

--- Assets/002. Scripts/Player/PlayerGunFire.cs
+++ Assets/002. Scripts/Player/PlayerGunFire.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,17 +12,16 @@ public class PlayerGunFire : MonoBehaviour
 
     private float _timer;
 
+    // 줌모드
     private const int DefaultFOV = 60;
     private const int ZoomFOV = 20;
-    private bool _isZoomMode = false;
+    private bool _isZoomMode = false;  
 
     public GameObject CrosshairUI;
     public GameObject CrosshairZoomUI;
 
-
     // 총을 담는 인벤토리
     public List<Gun> GunInventory;
-
 
     // 목표: 마우스 왼쪽 버튼을 누르면 시선이 바라보는 방향으로 총을 발사하고 싶다.
     // 필요 속성
@@ -31,11 +31,14 @@ public class PlayerGunFire : MonoBehaviour
     // - 총알 개수 텍스트 UI
     public Text BulletTextUI;
 
-    private bool _isReloading = false;      // 재장전 중이냐?
+    // 재장전
+    private bool _isReloading = false;      
     public GameObject ReloadTextObject;
 
     // 무기 이미지 UI
     public Image GunImageUI;
+
+
 
     private void Start()
     {
@@ -67,21 +70,25 @@ public class PlayerGunFire : MonoBehaviour
         _isReloading = false;
     }
 
+    // 줌 모드에 따라 카메라 FOV 수정해주는 메서드
+    private void RefreshZoomMode()
+    {
+        if (!_isZoomMode)
+        {
+            Camera.main.fieldOfView = DefaultFOV;
+        }
+        else
+        {
+            Camera.main.fieldOfView = ZoomFOV;
+        }
+    }
     private void Update()
     {
-        // 마우스 휠 버튼 눌렀을 때ㅔ && 현재 총이 스나이퍼
+        // 마우스 휠 버튼 눌렀을 때 && 현재 총이 스나이퍼
         if (Input.GetMouseButtonDown(2) && CurrentGun.GType == GunType.Sniper)
         {
-            if (_isZoomMode)
-            {
-                _isZoomMode = false;
-                Camera.main.fieldOfView = DefaultFOV;
-            }
-            else
-            {
-                _isZoomMode = true;
-                Camera.main.fieldOfView = ZoomFOV;
-            }
+            _isZoomMode = !_isZoomMode; // 줌 모드 뒤집기
+            RefreshZoomMode();
             RefreshUI();
         }
 
@@ -94,6 +101,8 @@ public class PlayerGunFire : MonoBehaviour
                 _currentGunIndex = GunInventory.Count - 1;
             }
             CurrentGun = GunInventory[_currentGunIndex];
+            _isZoomMode = false;
+            RefreshZoomMode();
             RefreshGun();
             RefreshUI();
         }
@@ -106,6 +115,8 @@ public class PlayerGunFire : MonoBehaviour
                 _currentGunIndex = 0;
             }
             CurrentGun = GunInventory[_currentGunIndex];
+            _isZoomMode = false;
+            RefreshZoomMode();
             RefreshGun();
             RefreshUI();
         }
@@ -113,6 +124,8 @@ public class PlayerGunFire : MonoBehaviour
         {
             _currentGunIndex = 0;
             CurrentGun = GunInventory[0];
+            _isZoomMode = false;
+            RefreshZoomMode();
             RefreshGun();
             RefreshUI();
         }
@@ -120,6 +133,8 @@ public class PlayerGunFire : MonoBehaviour
         {
             _currentGunIndex = 1;
             CurrentGun = GunInventory[1];
+            _isZoomMode = false;
+            RefreshZoomMode();
             RefreshGun();
             RefreshUI();
         }
@@ -127,6 +142,8 @@ public class PlayerGunFire : MonoBehaviour
         {
             _currentGunIndex = 2;
             CurrentGun = GunInventory[2];
+            _isZoomMode = false;
+            RefreshZoomMode();
             RefreshGun();
             RefreshUI();
         }
@@ -140,7 +157,6 @@ public class PlayerGunFire : MonoBehaviour
         }
 
         ReloadTextObject.SetActive(_isReloading);
-
 
         _timer += Time.deltaTime;
 
@@ -174,7 +190,6 @@ public class PlayerGunFire : MonoBehaviour
                     hitObject.Hit(CurrentGun.Damage);
                 }
 
-
                 // 5. 부딛힌 위치에 (총알이 튀는)이펙트를 위치한다.
                 HitEffect.gameObject.transform.position = hitInfo.point;
                 // 6. 이펙트가 쳐다보는 방향을 부딛힌 위치의 법선 벡터로 한다.
@@ -182,21 +197,12 @@ public class PlayerGunFire : MonoBehaviour
                 HitEffect.Play();
             }
         }
-
     }
 
     private void RefreshGun()
     {
         foreach (Gun gun in GunInventory)
         {
-            /**if (gun == CurrentGun)
-            {
-                gun.gameObject.SetActive(true);
-            }
-            else
-            {
-                gun.gameObject.SetActive(false);
-            }**/
             gun.gameObject.SetActive(gun == CurrentGun);
         }
     }
