3ee958b08bfa415941cf70ea2a618947bbf75bb9
SeungYeonKo <createNewScene479@gmail.com>
2024-02-23 10:01:57 +0900

    플레이어와 아이템의 거리가 가까워졌을 때 아이템이 자연스럽게(Slerp)날라오게 하기

--- Assets/002. Scripts/Item/ItemObject.cs
+++ Assets/002. Scripts/Item/ItemObject.cs
@@ -5,27 +5,29 @@ using UnityEngine;
 
 public enum ItemState
 {
-    Idle,
-    Flying
+    Idle,           // 대기 상태        (플레이어와의 거리를 체크한다)
+    Flying        // 날라오는 상태 (n초에 걸쳐서 slerp로 플레이어에게 날라온다)
 }
 public class ItemObject : MonoBehaviour
 {
     public ItemType ItemType;
-   
-    public Transform _target;
-    public float FindDistance = 7f;
-    public float MoveSpeed = 15f;
-
-    private CharacterController _characterController;
 
     private ItemState _CurrentState = ItemState.Idle;
+    public Transform _player;
+    public float FindDistance = 7f;           // 감지 거리  
+
+    private Vector3 _startPosition;
+    private const float FLYING_DURATION = 0.3f;
+    private float _progress = 0;
+
+
     // Todo 1. 아이템 프리팹을 3개만든다. 
     // Todo 2. 플레이어와 일정 거리가 되면 아이템이 먹어지고 사라진다
 
     private void Start()
     {
-        _characterController = GetComponent<CharacterController>();
-        _target = GameObject.FindGameObjectWithTag("Player").transform;
+        _player = GameObject.FindWithTag("Player").transform;     // 캐싱
+        _startPosition = transform.position;
     }
     private void Update()
     {
@@ -41,7 +43,10 @@ public class ItemObject : MonoBehaviour
     }
     private void Idle()
     {
-        if (Vector3.Distance(_target.position, transform.position) <= FindDistance)
+        // 대기 상태의 행동 : 플레이어와의 거리를 체크한다
+        float distance = Vector3.Distance(_player.position, transform.position);
+        // 전이 조건 : 플레이어와 거리가 충분히 가까워지면
+        if (distance <= FindDistance)
         {
             Debug.Log("상태 전환 : Idle -> Flying");
             _CurrentState = ItemState.Flying;
@@ -50,23 +55,17 @@ public class ItemObject : MonoBehaviour
 
     private void Flying()
     {
-        Vector3 dir = _target.transform.position - this.transform.position;
-        dir.Normalize();
+        _progress += Time.deltaTime / FLYING_DURATION;
+        transform.position = Vector3.Slerp(_startPosition, _player.position, _progress);
 
-        _characterController.Move(dir * MoveSpeed * Time.deltaTime);
-        transform.LookAt(_target);
-        Debug.Log("슝~ 날아가는중~");
-    }
-
-    private void OnTriggerEnter(Collider collider)
-    {
-        if (collider.CompareTag("Player"))
+        if (_progress >= 1)
         {
             // 아이템 매니저(인벤토리)에 추가하고, 
             ItemManager.Instance.AddItem(ItemType);
             // 사라진다
             gameObject.SetActive(false);
         }
+        Debug.Log("슝~ 날아가는중~");
     }
 }
     // 실습 과제 31. 몬스터가 죽으면 아이템이 드랍(Health: 30%, Stamina: 20%, Bullet: 10%)
